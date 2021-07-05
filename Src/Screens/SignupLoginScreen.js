782f4b0ccf478b572fe0417534e1fa15150ed52e
Abdelrahaman Saad <a.saad1@yodawy.com>
2021-07-05 04:06:17 +0200

    React Navigation Settlements

---
+++ Src/Screens/SignupLoginScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+    View,
+    StyleSheet
+} from 'react-native';
+
+class SignupLoginScreen extends Component {
+    render() {
+        return (
+            <View />
+        )
+    }
+}
+
+export { SignupLoginScreen };
+
+const styles = StyleSheet.create({});
