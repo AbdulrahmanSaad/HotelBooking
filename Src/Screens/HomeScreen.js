588ac922b859d6fb5400061f1bc0d3776e0b49d4
Abdelrahaman Saad <a.saad1@yodawy.com>
2021-07-08 20:54:41 +0200

    Second half of Home Screen

--- Src/Screens/HomeScreen.js
+++ Src/Screens/HomeScreen.js
@@ -4,11 +4,16 @@ import {
     Image,
     Text,
     StyleSheet,
+    FlatList,
     Dimensions
 } from 'react-native';
 import {
     HomeScreenTextbutton
 } from '../Components/Index'
+
+let reccomendData = [1, 2, 3, 4, 5, 6]
+let popularData = [1, 2, 3, 4, 5]
+let trendingData = [1, 2, 3, 4]
 class HomeScreen extends Component {
 
     constructor(props) {
@@ -16,7 +21,8 @@ class HomeScreen extends Component {
         this.state = {
             tab1Selected: true,
             tab2Selected: false,
-            tab3Selected: false
+            tab3Selected: false,
+            data: reccomendData
         }
     }
 
@@ -26,21 +32,24 @@ class HomeScreen extends Component {
                 this.setState({
                     tab1Selected: true,
                     tab2Selected: false,
-                    tab3Selected: false
+                    tab3Selected: false,
+                    data: reccomendData
                 })
                 break;
             case 2:
                 this.setState({
                     tab1Selected: false,
                     tab2Selected: true,
-                    tab3Selected: false
+                    tab3Selected: false,
+                    data: popularData
                 })
                 break;
             case 3:
                 this.setState({
                     tab1Selected: false,
                     tab2Selected: false,
-                    tab3Selected: true
+                    tab3Selected: true,
+                    data: trendingData
                 })
             default:
                 return;
@@ -51,13 +60,15 @@ class HomeScreen extends Component {
         const {
             tab1Selected,
             tab2Selected,
-            tab3Selected
+            tab3Selected,
+            data
         } = this.state;
         const {
             image,
             container,
             text,
             buttonsContainer,
+            imgItem,
         } = styles;
         return (
             <View
@@ -90,6 +101,17 @@ class HomeScreen extends Component {
                         onPress={() => this.tabSelected(3)}
                     />
                 </View>
+                <FlatList
+                    data={data}
+                    renderItem={() => (
+                        <Image
+                            source={require('../assets/img.png')}
+                            style={imgItem}
+                        />
+                    )}
+                    horizontal
+                    keyExtractor={item => item}
+                />
             </View>
         );
     }
@@ -118,28 +140,14 @@ const styles = StyleSheet.create({
         marginTop: 124,
         fontWeight: 'bold'
     },
-    button: {
-        justifyContent: 'center',
-        height: 30
-    },
     buttonsContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginTop: 50
     },
-    selectedView: {
-        width: 10,
-        height: 10,
-        backgroundColor: '#00a76e',
-        borderRadius: 10
-    },
-    selectedButtonView: {
-        alignItems: 'center',
-    },
-    buttonText: {
-        fontSize: 20,
-        color: '#b3b3b3',
-        fontWeight: 'bold'
+    imgItem: {
+        width: 256,
+        height: 424,
     }
 })
 
